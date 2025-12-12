9fa3a29cc5455a0268de7501a13e42c609277e17
MrBitasmblTester2 <lulu2214@proton.me>
2025-12-12 20:09:17 +0400

    Add +page.svelte

---
+++ svelte-frontend/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">let city="";</script>
+<section>
+ <input bind:value={city} placeholder="Search city" />
+ <button>Search</button>
+ <div id="current"></div>
+ <div id="forecast"></div>
+ <div id="charts"></div>
+</section>
